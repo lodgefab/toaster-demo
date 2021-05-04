044c8d141f4138d2090a300831c56fd82df47b7c
sorashin <naka52shin@gmail.com>
2021-05-04 22:03:41 +0900

    InputをonHoverで判別できるように変更

--- src/script.js
+++ src/script.js
@@ -24,6 +24,21 @@ const gui = new dat.GUI({
 const scene = new THREE.Scene()
 const scene2 = new THREE.Scene();
 
+//Init Elements
+let element, object, mesh
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+{
+     mouse.x = event.clientX / sizes.width * 2 - 1
+     mouse.y = - (event.clientY / sizes.height) * 2 + 1 
+    // console.log(mouse)
+})
+
 /**
  * Loaders
  */
@@ -51,6 +66,8 @@ bakedTexture.encoding = THREE.sRGBEncoding
  */
 // Baked Material
 const bakedMaterial = new THREE.MeshBasicMaterial({map:bakedTexture})
+const material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.FrontSide, transparent: true, opacity:0 } );
+
 
 /**
  * Models
@@ -59,7 +76,7 @@ gltfLoader.load(
     'toaster.glb',
     (gltf)=>{
         gltf.scene.traverse((child)=>{
-            console.log(child)
+            // console.log(child)
             child.material = bakedMaterial
         })
 
@@ -70,6 +87,15 @@ gltfLoader.load(
 )
 
 
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+let currentIntersect = null
+const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+const rayDirection = new THREE.Vector3(10, 0, 0)
+rayDirection.normalize()
+
 
 /**
  * Input
@@ -77,28 +103,30 @@ gltfLoader.load(
 
 function createInput( width, height, cssColor, pos, rot ) {
 
-    const element = document.createElement( 'input' );
+    element = document.createElement( 'input' );
     element.style.width = width + 'px';
-    element.style.height = height/10 + 'px';
+    element.style.height = height + 'px';
     element.style.opacity = 0.75;
     element.style.background = cssColor;
+    element.style.borderStyle = 'solid';
+    element.style.borderWidth = 0+'px';
 
-    const object = new CSS3DObject( element );
+    object = new CSS3DObject( element );
     object.position.copy( pos );
     object.rotation.copy( rot );
     scene2.add( object );
 
     // scene1追加用
-    // const geometry = new THREE.PlaneGeometry( width, height );
-    // const mesh = new THREE.Mesh( geometry, material );
-    // mesh.position.copy( object.position );
-    // mesh.rotation.copy( object.rotation );
-    // scene.add( mesh );
+    const geometry = new THREE.PlaneGeometry( width, height );
+    mesh = new THREE.Mesh( geometry, material );
+    mesh.position.copy( object.position );
+    mesh.rotation.copy( object.rotation );
+    scene.add( mesh );
 }
 
 // bottom
 createInput(
-    30, 30,
+    6, 1,
     'seagreen',
     new THREE.Vector3( 0, 0, 0 ),
     new THREE.Euler( - 90 * THREE.MathUtils.DEG2RAD, 0, 0 )
@@ -127,6 +155,7 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+
 
 /**
  * Camera
@@ -158,10 +187,8 @@ renderer2.domElement.style.position = 'absolute';
 renderer2.domElement.style.top = 0;
 document.body.appendChild( renderer2.domElement );
 
-// 
+
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
 
 const controls = new OrbitControls( camera, renderer2.domElement );
 controls.minZoom = 0.5;
@@ -177,6 +204,26 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    //Raycaster
+    raycaster.setFromCamera(mouse, camera)
+    const objectsToTest = [mesh]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+    if(intersects.length)
+    {
+        if(!currentIntersect)
+        {
+            console.log('mouse enter')
+        }
+        currentIntersect = intersects[0]
+    }
+    else
+    {
+        if(currentIntersect)
+        {
+            console.log('mouse leave')
+        }
+        currentIntersect = null
+    }
     // Update controls
     controls.update()
 
